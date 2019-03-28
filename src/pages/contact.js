d195c61dd9c389ec460196a32f3fc5e6433c16d5
ktichmann <ktichmann@gmail.com>
2019-03-28 21:05:00 +0200

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,114 +6,115 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import "../styles/loader.css";
 import "../styles/contact-page.css";
+
 class ContactPage extends React.Component {
-	componentDidMount() {
-		document.getElementById("contact-form").addEventListener("submit", e => {
-			document.getElementById("error_message").textContent = "";
-			e.preventDefault();
-			const email = document.getElementById("email").value;
-			const message = document.getElementById("message").value;
-			const errorMessageDiv = document.getElementById("error_message");
-			const contactFormDiv = document.getElementById("contact-form");
+  componentDidMount() {
+    document.getElementById("contact-form").addEventListener("submit", e => {
+      document.getElementById("error_message").textContent = "";
+      e.preventDefault();
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+      const errorMessageDiv = document.getElementById("error_message");
+      const contactFormDiv = document.getElementById("contact-form");
 
-			if (
-				email.match(
-					/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
-				)
-			) {
-				if (message) {
-					//hide the form, and show a loading image
-					contactFormDiv.style.display = "none";
-					document.getElementById("loader").classList.add("loader");
+      if (
+        email.match(
+          /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+        )
+      ) {
+        if (message) {
+          //hide the form, and show a loading image
+          contactFormDiv.style.display = "none";
+          document.getElementById("loader").classList.add("loader");
 
-					//send the form data
-					const form = new FormData(contactFormDiv);
-					const encodedForm = new URLSearchParams(form);
+          //send the form data
+          const form = new FormData(contactFormDiv);
+          const encodedForm = new URLSearchParams(form);
 
-					fetch("http://sultry-sloth.prod.with-datafire.io/contact", {
-						method: "POST",
-						body: encodedForm
-					})
-						.then(res => res.json())
-						.then(res => {
-							document.getElementById("loader").classList.remove("loader");
-							console.log(res);
-							if (res === "Thanks for getting in touch!") {
-								document.getElementById(
-									"form-wrapper"
-								).innerHTML = `<h3>Your message has been sent. <br> Thanks for getting in touch.</h3>`;
-							} else {
-								contactFormDiv.style.display = "block";
-								errorMessageDiv.textContent =
-									"There was an error sending your message, please try again, or email ktichmann@gmail.com";
-							}
-						});
-				} else {
-					errorMessageDiv.textContent =
-						"Please enter something in the message box.";
-				}
-			} else {
-				errorMessageDiv.textContent = "Please enter a valid email address.";
-			}
-		});
-	}
-	render() {
-		return (
-			<Layout>
-				<div style={{ overflow: "hidden" }} className={headStyle.headAboveFold}>
-					<div className={headStyle.overlay} />
-					<img
-						style={{ height: "auto", marginTop: "-300px" }}
-						className={headStyle.headerImg}
-						src={contactImg}
-						alt='computer with code'
-					/>
-					<div className={headStyle.headText}>
-						<h1>Get in Touch</h1>
-					</div>
-				</div>
-				<div id='form-wrapper'>
-					<span className='title'>Send me a message:</span>
-					<div id='error_message' />
-					<div id='loader' />
-					<form id='contact-form'>
-						<label htmlFor='emailAddress'>Email</label>
-						<TextField
-							margin='normal'
-							variant='outlined'
-							id='email'
-							name='emailAddress'
-						/>
-						<label htmlFor='message'>Message</label>
-						<TextField
-							multiline
-							rows='6'
-							margin='normal'
-							variant='outlined'
-							id='message'
-							name='message'
-						/>
-						<Button variant='contained' color='primary' type='submit'>
-							Send
-						</Button>
-					</form>
-				</div>
+          fetch("http://sultry-sloth.prod.with-datafire.io/contact", {
+            method: "POST",
+            body: encodedForm
+          })
+            .then(res => res.json())
+            .then(res => {
+              document.getElementById("loader").classList.remove("loader");
+              console.log(res);
+              if (res === "Thanks for getting in touch!") {
+                document.getElementById(
+                  "form-wrapper"
+                ).innerHTML = `<h3>Your message has been sent. <br> Thanks for getting in touch.</h3>`;
+              } else {
+                contactFormDiv.style.display = "block";
+                errorMessageDiv.textContent =
+                  "There was an error sending your message, please try again, or email ktichmann@gmail.com";
+              }
+            });
+        } else {
+          errorMessageDiv.textContent =
+            "Please enter something in the message box.";
+        }
+      } else {
+        errorMessageDiv.textContent = "Please enter a valid email address.";
+      }
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <div style={{ overflow: "hidden" }} className={headStyle.headAboveFold}>
+          <div className={headStyle.overlay} />
+          <img
+            style={{ height: "auto", marginTop: "-300px" }}
+            className={headStyle.headerImg}
+            src={contactImg}
+            alt="computer with code"
+          />
+          <div className={headStyle.headText}>
+            <h1>Get in Touch</h1>
+          </div>
+        </div>
+        <div id="form-wrapper">
+          <span className="title">Send me a message:</span>
+          <div id="error_message" />
+          <div id="loader" />
+          <form id="contact-form">
+            <label htmlFor="emailAddress">Email</label>
+            <TextField
+              margin="normal"
+              variant="outlined"
+              id="email"
+              name="emailAddress"
+            />
+            <label htmlFor="message">Message</label>
+            <TextField
+              multiline
+              rows="6"
+              margin="normal"
+              variant="outlined"
+              id="message"
+              name="message"
+            />
+            <Button variant="contained" color="primary" type="submit">
+              Send
+            </Button>
+          </form>
+        </div>
 
-				<div className='links'>
-					<span className='title'>Or Visit My:</span>
-					<a target='_blank' href='https://github.com/ktichmann'>
-						Github
-					</a>
-					<a target='_blank' href='https://codepen.io'>
-						CodePen
-					</a>
-					<a target='_blank' href='https://linkedin.com'>
-						LinkedIn
-					</a>
-				</div>
-			</Layout>
-		);
-	}
+        <div className="links">
+          <span className="title">Or Visit My:</span>
+          <a target="_blank" href="https://github.com/ktichmann">
+            Github
+          </a>
+          <a target="_blank" href="https://codepen.io">
+            CodePen
+          </a>
+          <a target="_blank" href="https://linkedin.com">
+            LinkedIn
+          </a>
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default ContactPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,85 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import headStyle from "../styles/pageHead.module.css"
+import Layout from "../components/layout";
+import headStyle from "../styles/pageHead.module.css";
 
-import avatar from "../images/avatar.png"
-import codepad from "../images/codepad.jpg"
-import movie from "../images/movie.png"
-import ssl from "../images/ssl.jpg"
-import forum from "../images/jobFinder.jpg"
-import avatarExcited from "../images/avatar-excited.png"
-import styles from "../styles/index.module.css"
+import avatar from "../images/avatar.png";
+import codepad from "../images/codepad.jpg";
+import movie from "../images/movie.png";
+import ssl from "../images/ssl.jpg";
+import forum from "../images/jobFinder.jpg";
+import avatarExcited from "../images/avatar-excited.png";
+import styles from "../styles/index.module.css";
 
 class IndexPage extends React.Component {
-  componentDidMount(){
-    let avatarEl = document.querySelector(`.${styles.avatar}`)
-    avatarEl.addEventListener("mouseover", () => {avatarEl.src = avatarExcited})
-    avatarEl.addEventListener("mouseleave", () => {avatarEl.src = avatar})
+  componentDidMount() {
+    let avatarEl = document.querySelector(`.${styles.avatar}`);
+    avatarEl.addEventListener("mouseover", () => {
+      avatarEl.src = avatarExcited;
+    });
+    avatarEl.addEventListener("mouseleave", () => {
+      avatarEl.src = avatar;
+    });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <Layout>
         <div className={headStyle.headAboveFold} id={styles.indexAboveFold}>
-          <div style={{
-            marginTop: "14%",
-          }}>
-            <h1 style={{textAlign: "left"}}>Hi, I'm Kerwin</h1>
-            <h3 style={{textAlign: "left"}}>I'm a front-end web developer based in Cape Town. <br />Take a look at my <Link to="/projects">projects</Link> or <Link to="/contact">get&nbsp;in&nbsp;touch</Link></h3>
+          <div
+            style={{
+              marginTop: "14%"
+            }}
+          >
+            <h1 style={{ textAlign: "left" }}>Hi, I'm Kerwin</h1>
+            <h3 style={{ textAlign: "left" }}>
+              I'm a front-end web developer based in Cape Town. <br />
+              Take a look at my <Link to="/projects">projects</Link> or
+              <Link to="/contact"> get&nbsp;in&nbsp;touch</Link>
+            </h3>
           </div>
-          <img alt="avatar" className={styles.avatar} src={avatar}/>
+          <img alt="avatar" className={styles.avatar} src={avatar} />
         </div>
         <div className={styles.project} id={styles.codepad}>
-          <div className={styles.projectOverlay}> 
-          <p>CodePad - A code note-taking extension for chrome with syntax highlighting, prettify & emmet support </p>
+          <div className={styles.projectOverlay}>
+            <p>
+              For'm - A decoupled forum app with an api run on Node & Postgres,
+              and a frontend built in React/Gatsby
+            </p>
+          </div>
+          <img alt="for'm" src={forum} />
+        </div>
+        <div className={styles.project} id={styles.codepad}>
+          <div className={styles.projectOverlay}>
+            <p>
+              Let's Helper - A WordPress Plugin that allows users to create,
+              update, list, and revoke SSL certificates.
+            </p>
+          </div>
+          <img style={{ width: "100%" }} alt="lets-helper" src={ssl} />
+        </div>
+        <div className={styles.project} id={styles.codepad}>
+          <div className={styles.projectOverlay}>
+            <p>
+              CodePad - A code note-taking extension for chrome with syntax
+              highlighting, prettify & emmet support
+            </p>
           </div>
           <img alt="codepad" src={codepad} />
         </div>
         <div className={styles.project} id={styles.codepad}>
-          <div className={styles.projectOverlay}> 
-          <p>CodePad - A code note-taking extension for chrome with syntax highlighting, prettify & emmet support </p>
+          <div className={styles.projectOverlay}>
+            <p>
+              Movie Summarizer - An app that pulls scripts from IMSDB, and
+              returns a 3-line summary using TF-IDF.
+            </p>
           </div>
           <img alt="movie-summarizer" src={movie} />
         </div>
-        <div className={styles.project} id={styles.codepad}>
-          <div className={styles.projectOverlay}> 
-          <p>CodePad - A code note-taking extension for chrome with syntax highlighting, prettify & emmet support </p>
-          </div>
-          <img style={{width: "100%"}} alt="lets-helper" src={ssl} />
-        </div>
-        <div className={styles.project} id={styles.codepad}>
-          <div className={styles.projectOverlay}> 
-          <p>For'm - A decoupled forum app with an api run on Node & Postgres, and a frontend built in React/Gatsby</p>
-          </div>
-          <img alt="for'm" src={forum} />
-        </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
