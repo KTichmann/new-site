9ed33262393b095eba754d2f7c59e0377c873e81
unknown <ktichmann@gmail.com>
2019-07-16 15:10:35 +0200

    add img differ project

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,90 +6,90 @@ import headStyle from "../styles/pageHead.module.css";
 
 import avatar from "../images/avatar.png";
 import codepad from "../images/codepad.jpg";
-import movie from "../images/movie.png";
+import imgDiff from "../images/imgDiff.png";
 import ssl from "../images/ssl.jpg";
 import forum from "../images/jobFinder.jpg";
 import avatarExcited from "../images/avatar-excited.png";
 import styles from "../styles/index.module.css";
 
 class IndexPage extends React.Component {
-  componentDidMount() {
-    let avatarEl = document.querySelector(`.${styles.avatar}`);
-    avatarEl.addEventListener("mouseover", () => {
-      avatarEl.src = avatarExcited;
-    });
-    avatarEl.addEventListener("mouseleave", () => {
-      avatarEl.src = avatar;
-    });
-  }
+	componentDidMount() {
+		let avatarEl = document.querySelector(`.${styles.avatar}`);
+		avatarEl.addEventListener("mouseover", () => {
+			avatarEl.src = avatarExcited;
+		});
+		avatarEl.addEventListener("mouseleave", () => {
+			avatarEl.src = avatar;
+		});
+	}
 
-  render() {
-    return (
-      <Layout>
-        <div className={headStyle.headAboveFold} id={styles.indexAboveFold}>
-          <div
-            style={{
-              marginTop: "14%",
-              paddingLeft: "1rem"
-            }}
-          >
-            <h1 style={{ textAlign: "left" }}>Hi, I'm Kerwin</h1>
-            <h3 style={{ textAlign: "left" }}>
-              I'm a front-end web developer based in Cape Town. <br />
-              Take a look at my <Link to="/projects">projects</Link> or
-              <Link to="/contact"> get&nbsp;in&nbsp;touch</Link>
-            </h3>
-          </div>
-          <img alt="avatar" className={styles.avatar} src={avatar} />
-        </div>
-        <div className={styles.project}>
-          <Link to="/projects#forumApi">
-            <div className={styles.projectOverlay}>
-              <p>
-                For'm - A decoupled forum app with an api run on Node &
-                Postgres, and a frontend built in React/Gatsby
-              </p>
-            </div>
-            <img alt="for'm" src={forum} />
-          </Link>
-        </div>
-        <div className={styles.project}>
-          <Link to="/projects#letshelper">
-            <div className={styles.projectOverlay}>
-              <p>
-                Let's Helper - A WordPress Plugin that allows users to create,
-                update, list, and revoke SSL certificates.
-              </p>
-            </div>
-            <img style={{ width: "100%" }} alt="lets-helper" src={ssl} />
-          </Link>
-        </div>
-        <div className={styles.project}>
-          <Link to="/projects#codePad">
-            <div className={styles.projectOverlay}>
-              <p>
-                CodePad - A code note-taking extension for chrome with syntax
-                highlighting, prettify & emmet support
-              </p>
-            </div>
-            <img alt="codepad" src={codepad} />
-          </Link>
-        </div>
+	render() {
+		return (
+			<Layout>
+				<div className={headStyle.headAboveFold} id={styles.indexAboveFold}>
+					<div
+						style={{
+							marginTop: "14%",
+							paddingLeft: "1rem"
+						}}>
+						<h1 style={{ textAlign: "left" }}>Hi, I'm Kerwin</h1>
+						<h3 style={{ textAlign: "left" }}>
+							I'm a front-end web developer based in Cape Town. <br />
+							Take a look at my <Link to='/projects'>projects</Link> or
+							<Link to='/contact'> get&nbsp;in&nbsp;touch</Link>
+						</h3>
+					</div>
+					<img alt='avatar' className={styles.avatar} src={avatar} />
+				</div>
+				<div className={styles.project}>
+					<Link to='/projects#forumApi'>
+						<div className={styles.projectOverlay}>
+							<p>
+								For'm - A decoupled forum app with an api run on Node &
+								Postgres, and a frontend built in React/Gatsby
+							</p>
+						</div>
+						<img alt="for'm" src={forum} />
+					</Link>
+				</div>
+				<div className={styles.project}>
+					<Link to='/projects#letshelper'>
+						<div className={styles.projectOverlay}>
+							<p>
+								Let's Helper - A WordPress Plugin that allows users to create,
+								update, list, and revoke SSL certificates.
+							</p>
+						</div>
+						<img style={{ width: "100%" }} alt='lets-helper' src={ssl} />
+					</Link>
+				</div>
+				<div className={styles.project}>
+					<Link to='/projects#codePad'>
+						<div className={styles.projectOverlay}>
+							<p>
+								CodePad - A code note-taking extension for chrome with syntax
+								highlighting, prettify & emmet support
+							</p>
+						</div>
+						<img alt='codepad' src={codepad} />
+					</Link>
+				</div>
 
-        <div className={styles.project}>
-          <Link to="/projects#movieSummarizer">
-            <div className={styles.projectOverlay}>
-              <p>
-                Movie Summarizer - An app that pulls scripts from IMSDB, and
-                returns a 3-line summary using TF-IDF.
-              </p>
-            </div>
-            <img alt="movie-summarizer" src={movie} />
-          </Link>
-        </div>
-      </Layout>
-    );
-  }
+				<div className={styles.project}>
+					<Link to='/projects#imgDiff'>
+						<div className={styles.projectOverlay}>
+							<p>
+								Img-Diff - an image comparison SPA that supports uploading
+								images to compare differences, as well as capturing images from
+								urls.
+							</p>
+						</div>
+						<img alt='image differ project' src={imgDiff} />
+					</Link>
+				</div>
+			</Layout>
+		);
+	}
 }
 
 export default IndexPage;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,7 +6,7 @@ import styles from "../styles/projects.module.css";
 import projectHeader from "../images/projectHeader.jpg";
 
 import codepad from "../images/codepad.jpg";
-import movie from "../images/movie.png";
+import imgDiff from "../images/imgDiff2.png";
 import ssl from "../images/ssl.jpg";
 import forum from "../images/forum-img.png";
 import api from "../images/forumApi.jpg";
@@ -24,252 +24,243 @@ import scss from "../images/scss.png";
 import reactRouter from "../images/react-router.png";
 
 const PortfolioPage = () => (
-  <Layout>
-    <div className={headStyle.headAboveFold}>
-      <div className={headStyle.overlay} />
-      <img
-        className={headStyle.headerImg}
-        src={projectHeader}
-        alt="computer with code"
-      />
-      <div className={headStyle.headText}>
-        <h1>
-          Talk is Cheap, <br /> Show Me the Code
-        </h1>
-        <h3 style={{ textAlign: "right" }}>-Linus Torvalds</h3>
-      </div>
-    </div>
-    <div id={styles.projectSection}>
-      <div id="forumApi" className={styles.project}>
-        <div className={styles.imgWrapper}>
-          <img alt="forum-api" src={api} />
-          <div className={styles.imgOverlay}>
-            <div className={styles.builtWith}>
-              <ul style={{ textAlign: "center" }}>
-                <li>Built With: </li>
-                <li>
-                  <img alt="node" src={node} />
-                  Node
-                </li>
-                <li>
-                  <img alt="postgres" src={postgres} />
-                  PostgreSQL
-                </li>
-                <li>
-                  <img alt="heroku" src={heroku} />
-                  Heroku
-                </li>
-                <li>Express</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className={styles.projectSectionWording}>
-          <div className={styles.projectSectionHeader}>Forum API</div>
-          <span className={styles.separator} />
-          <span className={styles.projectSectionText}>
-            A headless forum api built using Express, Node, and saving data to a
-            PostGreSQL database. The api comes with routes for creating,
-            reading, updating, and deleting posts, likes, and comments. There's
-            also user authentication through JWT headers.
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://github.com/KTichmann/forum-api"
-            >
-              View on Github
-            </a>
-          </span>
-        </div>
-      </div>
-      <div id="forumFrontend" className={styles.projectReverse}>
-        <div className={styles.projectSectionWording}>
-          <div className={styles.projectSectionHeader}>Forum Frontend</div>
-          <span className={styles.separator} />
-          <span className={styles.projectSectionText}>
-            A frontend that consumes my Forum API, displaying a forum 'static'
-            site built in Gatsby. Users can sign up, log in, create posts,
-            comments, and leave likes. There's also a chat-box app built-in
-            that's built using websockets on a separate server.
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://ktichmann.github.io/forum-frontend"
-            >
-              See the Demo
-            </a>
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://github.com/KTichmann/forum-frontend"
-            >
-              View on Github
-            </a>
-          </span>
-        </div>
-        <div className={styles.imgWrapper}>
-          <img alt="forum" src={forum} />
-          <div className={styles.imgOverlay}>
-            <div className={styles.builtWith}>
-              <ul style={{ textAlign: "center" }}>
-                <li>Built With: </li>
-                <li>
-                  <img alt="node" src={gatsby} />
-                  Gatsby
-                </li>
-                <li>
-                  <img alt="node" src={react} />
-                  React
-                </li>
-                <li>
-                  <img alt="node" src={socket} />
-                  Socket.io
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="letshelper" className={styles.project}>
-        <div className={styles.imgWrapper}>
-          <img alt="ssl" src={ssl} />
-          <div className={styles.imgOverlay}>
-            <div className={styles.builtWith}>
-              <ul style={{ textAlign: "center" }}>
-                <li>Built With: </li>
-                <li>
-                  <img alt="wordpress" src={wordpress} />
-                  WordPress
-                </li>
-                <li>
-                  <img alt="php" src={php} />
-                  PHP
-                </li>
-                <li>
-                  <img alt="mysql" src={mysql} />
-                  MySQL
-                </li>
-                <li>
-                  <img alt="certbot" src={certbot} />
-                  Certbot-auto
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className={styles.projectSectionWording}>
-          <div className={styles.projectSectionHeader}>
-            LetsEncrypt Helper Plugin for WordPress
-          </div>
-          <span className={styles.separator} />
-          <span className={styles.projectSectionText}>
-            A wordpress plugin that allows website owners to manage their ssl
-            certificate through the wordpress frontend. Built for multisite
-            installations, the plugin runs certbot-auto on the backend to
-            create, update, revoke, or list certificates on the network.
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://github.com/KTichmann/Lets-Helper"
-            >
-              View on Github
-            </a>
-          </span>
-        </div>
-      </div>
-      <div id="codePad" className={styles.projectReverse}>
-        <div className={styles.projectSectionWording}>
-          <div className={styles.projectSectionHeader}>
-            CodePad - Chrome Extension
-          </div>
-          <span className={styles.separator} />
-          <span className={styles.projectSectionText}>
-            A note-taking chrome extension designed for taking quick front-end
-            code notes. Supports syntax highlighting for JavaScript, CSS, HTML,
-            CoffeeScript, and JSX, as well as code formatting with prettier, and
-            EMMET for HTML shortcuts.
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://github.com/KTichmann/CodePad"
-            >
-              View on Github
-            </a>
-          </span>
-        </div>
-        <div className={styles.imgWrapper}>
-          <img alt="codepad" src={codepad} />
-          <div className={styles.imgOverlay}>
-            <div className={styles.builtWith}>
-              <ul style={{ textAlign: "center" }}>
-                <li>Built With: </li>
-                <li>
-                  <img src={react} />
-                  React
-                </li>
-                <li>
-                  <img src={scss} />
-                  Scss
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="movieSummarizer" className={styles.project}>
-        <div className={styles.imgWrapper}>
-          <img alt="movie" src={movie} />
-          <div className={styles.imgOverlay}>
-            <div className={styles.builtWith}>
-              <ul style={{ textAlign: "center" }}>
-                <li>Built With: </li>
-                <li>
-                  <img src={node} />
-                  Node
-                </li>
-                <li>
-                  <img src={react} />
-                  React
-                </li>
-                <li>
-                  <img src={reactRouter} />
-                  React Router
-                </li>
-                <li>
-                  <img src={scss} />
-                  Scss
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>{" "}
-        <div className={styles.projectSectionWording}>
-          <div className={styles.projectSectionHeader}>Movie Summarizer</div>
-          <span className={styles.separator} />
-          <span className={styles.projectSectionText}>
-            A movie summarizing web-based app that allows users to search for
-            and select a movie to be summarized, then presents a 3-line summary
-            of the film. The app was built in two parts, the backend “API”,
-            hosted on Heroku, and the “static” front-end on Github Pages.
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://ktichmann.github.io/MovieSummarizer/#/"
-            >
-              See the Demo
-            </a>
-            <a
-              target="_blank"
-              style={{ display: "block" }}
-              href="https://github.com/KTichmann/MovieSummarizer"
-            >
-              View on Github
-            </a>
-          </span>
-        </div>
-      </div>
-    </div>
-  </Layout>
+	<Layout>
+		<div className={headStyle.headAboveFold}>
+			<div className={headStyle.overlay} />
+			<img
+				className={headStyle.headerImg}
+				src={projectHeader}
+				alt='computer with code'
+			/>
+			<div className={headStyle.headText}>
+				<h1>
+					Talk is Cheap, <br /> Show Me the Code
+				</h1>
+				<h3 style={{ textAlign: "right" }}>-Linus Torvalds</h3>
+			</div>
+		</div>
+		<div id={styles.projectSection}>
+			<div id='forumApi' className={styles.project}>
+				<div className={styles.imgWrapper}>
+					<img alt='forum-api' src={api} />
+					<div className={styles.imgOverlay}>
+						<div className={styles.builtWith}>
+							<ul style={{ textAlign: "center" }}>
+								<li>Built With: </li>
+								<li>
+									<img alt='node' src={node} />
+									Node
+								</li>
+								<li>
+									<img alt='postgres' src={postgres} />
+									PostgreSQL
+								</li>
+								<li>
+									<img alt='heroku' src={heroku} />
+									Heroku
+								</li>
+								<li>Express</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className={styles.projectSectionWording}>
+					<div className={styles.projectSectionHeader}>Forum API</div>
+					<span className={styles.separator} />
+					<span className={styles.projectSectionText}>
+						A headless forum api built using Express, Node, and saving data to a
+						PostGreSQL database. The api comes with routes for creating,
+						reading, updating, and deleting posts, likes, and comments. There's
+						also user authentication through JWT headers.
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://github.com/KTichmann/forum-api'>
+							View on Github
+						</a>
+					</span>
+				</div>
+			</div>
+			<div id='forumFrontend' className={styles.projectReverse}>
+				<div className={styles.projectSectionWording}>
+					<div className={styles.projectSectionHeader}>Forum Frontend</div>
+					<span className={styles.separator} />
+					<span className={styles.projectSectionText}>
+						A frontend that consumes my Forum API, displaying a forum 'static'
+						site built in Gatsby. Users can sign up, log in, create posts,
+						comments, and leave likes. There's also a chat-box app built-in
+						that's built using websockets on a separate server.
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://ktichmann.github.io/forum-frontend'>
+							See the Demo
+						</a>
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://github.com/KTichmann/forum-frontend'>
+							View on Github
+						</a>
+					</span>
+				</div>
+				<div className={styles.imgWrapper}>
+					<img alt='forum' src={forum} />
+					<div className={styles.imgOverlay}>
+						<div className={styles.builtWith}>
+							<ul style={{ textAlign: "center" }}>
+								<li>Built With: </li>
+								<li>
+									<img alt='node' src={gatsby} />
+									Gatsby
+								</li>
+								<li>
+									<img alt='node' src={react} />
+									React
+								</li>
+								<li>
+									<img alt='node' src={socket} />
+									Socket.io
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id='letshelper' className={styles.project}>
+				<div className={styles.imgWrapper}>
+					<img alt='ssl' src={ssl} />
+					<div className={styles.imgOverlay}>
+						<div className={styles.builtWith}>
+							<ul style={{ textAlign: "center" }}>
+								<li>Built With: </li>
+								<li>
+									<img alt='wordpress' src={wordpress} />
+									WordPress
+								</li>
+								<li>
+									<img alt='php' src={php} />
+									PHP
+								</li>
+								<li>
+									<img alt='mysql' src={mysql} />
+									MySQL
+								</li>
+								<li>
+									<img alt='certbot' src={certbot} />
+									Certbot-auto
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className={styles.projectSectionWording}>
+					<div className={styles.projectSectionHeader}>
+						LetsEncrypt Helper Plugin for WordPress
+					</div>
+					<span className={styles.separator} />
+					<span className={styles.projectSectionText}>
+						A wordpress plugin that allows website owners to manage their ssl
+						certificate through the wordpress frontend. Built for multisite
+						installations, the plugin runs certbot-auto on the backend to
+						create, update, revoke, or list certificates on the network.
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://github.com/KTichmann/Lets-Helper'>
+							View on Github
+						</a>
+					</span>
+				</div>
+			</div>
+			<div id='codePad' className={styles.projectReverse}>
+				<div className={styles.projectSectionWording}>
+					<div className={styles.projectSectionHeader}>
+						CodePad - Chrome Extension
+					</div>
+					<span className={styles.separator} />
+					<span className={styles.projectSectionText}>
+						A note-taking chrome extension designed for taking quick front-end
+						code notes. Supports syntax highlighting for JavaScript, CSS, HTML,
+						CoffeeScript, and JSX, as well as code formatting with prettier, and
+						EMMET for HTML shortcuts.
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://github.com/KTichmann/CodePad'>
+							View on Github
+						</a>
+					</span>
+				</div>
+				<div className={styles.imgWrapper}>
+					<img alt='codepad' src={codepad} />
+					<div className={styles.imgOverlay}>
+						<div className={styles.builtWith}>
+							<ul style={{ textAlign: "center" }}>
+								<li>Built With: </li>
+								<li>
+									<img src={react} />
+									React
+								</li>
+								<li>
+									<img src={scss} />
+									Scss
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id='imgDiff' className={styles.project}>
+				<div className={styles.imgWrapper}>
+					<img alt='imgDiff' src={imgDiff} />
+					<div className={styles.imgOverlay}>
+						<div className={styles.builtWith}>
+							<ul style={{ textAlign: "center" }}>
+								<li>Built With: </li>
+								<li>
+									<img src={node} />
+									Node
+								</li>
+								<li>
+									<img src={react} />
+									React
+								</li>
+								<li>
+									<img src={reactRouter} />
+									React Router
+								</li>
+								<li>Create-react-app</li>
+							</ul>
+						</div>
+					</div>
+				</div>{" "}
+				<div className={styles.projectSectionWording}>
+					<div className={styles.projectSectionHeader}>Image Differ</div>
+					<span className={styles.separator} />
+					<span className={styles.projectSectionText}>
+						An Image diffing single-page app that allows users to upload two
+						images, and compare them. The frontend sends the images to the
+						server, which uses a node image-comparison package, then draws
+						rectangles on the images to show where the differences are before
+						sending the response images.
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://ktichmann.github.io/imgDiffer'>
+							See the Demo
+						</a>
+						<a
+							target='_blank'
+							style={{ display: "block" }}
+							href='https://github.com/KTichmann/imgDiffer'>
+							View on Github
+						</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	</Layout>
 );
 
 export default PortfolioPage;
